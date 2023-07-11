16b1727c9fd9d0e158703165009958f02e9d25f4
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-07-11 21:16:19 +0300

    add basic websocket related functions

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,57 @@
-// TODO implement main function
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+// Define upgrader
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// For testing purposes all connection will be approved
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// Define reader to listen to our websocket endpoints
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
+
+// Define websocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Simple Server")
+	})
+	http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+	fmt.Println("Chat application v0.1")
+	setupRoutes()
+	http.ListenAndServe("localhost:8080", nil)
+}
